c525216abb6ad91c817d1fcba15c9f4892bd019f
Roosevelt Seymour <RSeymourJR04@outlook.com>
2023-02-16 23:49:12 +0000

    Completed Contains Challenge

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -13,7 +13,7 @@ public class Contains {
     public boolean arrayContains(int[] arr, int target){
         boolean found = false;
         for (int i = 0; i < arr.length; i++){
-            if(target == arr[i]){ found = true;}
+            if(target == arr[i]){ found = true; }
         }
         return found;
     }
